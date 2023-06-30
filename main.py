82e21d6a1ef46bc330059cfc87e7646baadb015c
gongshuai <892781323@qq.com>
2023-06-30 15:21:37 +0800

    samples

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from optimization.arguments import get_arguments
 from optimization.attribute_editor import AttributeEditor
 import sys
+import torch
 sys.path.append(".")
 sys.path.append("..")
 
@@ -11,3 +12,8 @@ if __name__ == '__main__':
     # attribute_editor.edit_image_for_person()
     attribute_editor.edit_image_by_attribute()
     # attribute_editor.sample_batch()
+
+    # path = '/training_data/CelebA/celeba-hq-30000/celeba-256-tmp/attribute_20_30000.pt'
+    # attributes = torch.load(path)
+    # print(f'attributes[8581] = {attributes[8581]}')
+    # print(f'attributes[20344] = {attributes[20344]}')
